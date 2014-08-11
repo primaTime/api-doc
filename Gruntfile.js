c106b98a6f2e92c67e29ed6538b2e314440ab67e
Petr Vojta <petr.vojta@abra.eu>
2014-08-11 10:37:16 +0200

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                input: "./",
+                github: "primaerp/api-doc"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '_book'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        },
+        'watch': {
+            coffee: {
+                files: ['./**/*.md','!./_book/**'],
+                tasks: ['gitbook']
+            }
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+
+    grunt.registerTask('default', 'gitbook');
+};
